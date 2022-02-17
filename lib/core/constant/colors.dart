badd4671e017fcb8853296fbf07a397016758a1d
Dimas <dimasnur.dev@gmail.com>
2022-02-18 00:29:01 +0700

    configure theming app

---
+++ lib/core/constant/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFF5077DF);
+
+  static const Color black = Colors.black;
+  static const Color white = Colors.white;
+}
